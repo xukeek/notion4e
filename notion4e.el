44d805ad522fca9d8c1d1653e78f35d21b8d9102
xukeek <okeoke1011@gmail.com>
2022-01-29 00:11:22 +0800

    init

---
+++ notion4e.el
@@ -0,0 +1,25 @@
+;;; notion4e.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2022 xukeek
+;;
+;; Author: xukeek <https://github.com/xukeek>
+;; Maintainer: xukeek <okeoke1011@gmail.com>
+;; Created: January 29, 2022
+;; Modified: January 29, 2022
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/xukeek/notion4e
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'notion4e)
+;;; notion4e.el ends here
